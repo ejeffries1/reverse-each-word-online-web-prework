54b641459341182c59ec991d9c1b31685ce09d3d
Evan Jeffries <ejeffries14@gmail.com>
2019-06-27 18:39:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(string)
    string_array = string.reverse.split()
    array = []
-   string_array.each do |arr|
-     array << string_array.pop()
+   string_array.each do |word|
+     array << word.reverse
    end
    return array.join(" ")
 end
